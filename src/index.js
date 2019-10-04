711b3c150738944a6341a6338340f12b9efb78e1
Pavel <pavel.rtnv@gmail.com>
2019-10-04 16:45:20 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function zeros(expression) {
-  // your solution
+  const operands = expression.split('*');
+  let sumFive = 0;
+  let sumTwo = 0;
+  for (const operand of operands) {
+    const arr =  zeroSumForOneOperand(parseInt(operand, 10), operand.includes('!!'));
+    sumFive += arr[0];
+    sumTwo += arr[1];
+  }
+
+  return (Math.min(sumFive, sumTwo));
+}
+
+
+function zeroSumForOneOperand(number, doubleFactorial = false) {
+  let counterFive = 0;
+  let counterTwo = 0;
+  for (
+        let i = (!(number % 2) && doubleFactorial) ? 2 : 1 ;
+        i <= number;
+        i += (doubleFactorial ? 2 : 1)
+      ) {
+          counterFive += getSumNumber(i, 5);
+          counterTwo += getSumNumber(i, 2); 
+  }
+  return [counterFive, counterTwo];
+}
+
+function getSumNumber(number, base) {
+  if (number % base) {
+    return 0;
+  } else {
+    return 1 + getSumNumber(number / base, base);
+  }
 }
